e5bd003a1c7747db9ab96dde0528f1c8b20f536f
Eappen Nelluvelil <eappen@tamu.edu>
2025-04-05 16:45:42 -0500

    Made the following changes: - Modified CBC FLUDS constructor body to size `local_psi_data_`  to have as many entries there are in `psi_new_local_[groupset.id]`. For the time being, this is not optimized to store the number of angles in the current angletset * the number of groups in groupset for the current angleset, but this is not drastically change the memory usage of the Sweep code. Currently, the `local_psi_data_` vector is allocated to store as many entries as there are local DOFS, obtained from the angular flux unknown manager `psi_uk_man_` - Implemented `GetLocalUpwindPsi` to return the appropriate subset of `local_psi_data_` to read local upwind angular flux data for incoming face surface integrals during a sweep - Implemented `GetLocalDownwindPsi` to return the appropriate subset of `local_psi_data_` to write to the current cell's just computed angular flux data during the sweep

--- modules/linear_boltzmann_solvers/discrete_ordinates_solver/sweep/fluds/cbc_fluds.cc
+++ modules/linear_boltzmann_solvers/discrete_ordinates_solver/sweep/fluds/cbc_fluds.cc
@@ -12,15 +12,18 @@ namespace opensn
 CBC_FLUDS::CBC_FLUDS(size_t num_groups,
                      size_t num_angles,
                      const CBC_FLUDSCommonData& common_data,
-                     std::vector<double>& local_psi_data,
                      const UnknownManager& psi_uk_man,
                      const SpatialDiscretization& sdm)
   : FLUDS(num_groups, num_angles, common_data.GetSPDS()),
     common_data_(common_data),
-    local_psi_data_(local_psi_data),
     psi_uk_man_(psi_uk_man),
     sdm_(sdm)
 {
+  // Need to be sized to hold angles in a angleset in a groupset, instead of all angles in all anglesets in the groupset
+  // i.e., num angles in angle set * num groups in groupset for that angle set
+  // For the time being, use the full vector
+  size_t num_ang_unknowns = sdm.GetNumLocalDOFs(psi_uk_man); 
+  local_psi_data_.assign(num_ang_unknowns, 0.0);
 }
 
 const FLUDSCommonData&
@@ -29,17 +32,40 @@ CBC_FLUDS::GetCommonData() const
   return common_data_;
 }
 
+// OLD METHOD: deprecate at some point
 const std::vector<double>&
 CBC_FLUDS::GetLocalUpwindDataBlock() const
 {
   return local_psi_data_;
 }
 
+// OLD METHOD: deprecate at some point
 const double*
 CBC_FLUDS::GetLocalCellUpwindPsi(const std::vector<double>& psi_data_block, const Cell& cell)
 {
   const auto dof_map = sdm_.MapDOFLocal(cell, 0, psi_uk_man_, 0, 0);
   return &psi_data_block[dof_map];
+}
+
+// NEW METHOD to avoid having to access psi_new_local_[groupset.id]
+const double*
+CBC_FLUDS::GetLocalUpwindPsi(const Cell& face_neighbor, 
+                              const unsigned int adj_cell_node_offset)
+                              const 
+{
+  // Starting index for upwind cell's angular flux data 
+  const auto dof_map = sdm_.MapDOFLocal(face_neighbor, 0, psi_uk_man_, 0, 0);
+  
+  const auto local_face_upwind_psi = &local_psi_data_[dof_map];
+  return &local_face_upwind_psi[adj_cell_node_offset];
+}
+
+// NEW METHOD to set angular value data for a downwind face of the cell
+double*
+CBC_FLUDS::GetLocalDownwindPsi(const Cell& cell)                
+{
+  const auto dof_map = sdm_.MapDOFLocal(cell, 0, psi_uk_man_, 0, 0);
+  return &local_psi_data_[dof_map];
 }
 
 const std::vector<double>&
@@ -55,38 +81,6 @@ CBC_FLUDS::GetNonLocalUpwindPsi(const std::vector<double>& psi_data,
 {
   const size_t dof_map = face_node_mapped * num_groups_and_angles_ + angle_set_index * num_groups_;
   return &psi_data[dof_map];
-}
-
-void CBC_FLUDS::UpdateCurrentCellPsiSubset(const double* psi_vector_start, size_t subset_size)
-{
-  // Resize the internal storage if necessary
-  if (current_cell_psi_subset_.size() != subset_size)
-  {
-    current_cell_psi_subset_.resize(subset_size);
-  }
-
-  // Copy the data from the main psi vector into the local subset storage
-  if (subset_size > 0 && psi_vector_start != nullptr)
-  {
-    // Using std::copy
-    std::copy(psi_vector_start,
-              psi_vector_start + subset_size,
-              current_cell_psi_subset_.begin());
-  }
-  else if (subset_size == 0)
-  {
-     current_cell_psi_subset_.clear(); // Handle zero size case if needed
-  }
-}
-
-const std::vector<double>& CBC_FLUDS::GetCurrentCellPsiSubset() const
-{
-  return current_cell_psi_subset_;
-}
-
-std::vector<double>& CBC_FLUDS::GetCurrentCellPsiSubset()
-{
-  return current_cell_psi_subset_;
 }
 
 } // namespace opensn
