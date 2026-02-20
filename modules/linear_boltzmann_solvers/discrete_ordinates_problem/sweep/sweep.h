1a587fd64c0807357b36be3140cda1b36dcddd02
Eappen Nelluvelil <eappen@tamu.edu>
2026-02-20 11:17:40 -0800

    Some merge fixes and temp. fixes for `CBC_AsynchronousCommunicator` to prevent corrupted, thread-unsafe access to `outgoing_message_queue_`

--- modules/linear_boltzmann_solvers/discrete_ordinates_problem/sweep/sweep.h
+++ modules/linear_boltzmann_solvers/discrete_ordinates_problem/sweep/sweep.h
@@ -1,3 +1,62 @@
+// // SPDX-FileCopyrightText: 2024 The OpenSn Authors <https://open-sn.github.io/opensn/>
+// // SPDX-License-Identifier: MIT
+
+// #pragma once
+
+// #include <vector>
+// #include <set>
+// #include <memory>
+
+// namespace opensn
+// {
+
+// class Cell;
+// class SPDS;
+// class MeshContinuum;
+
+// enum class FaceOrientation : short
+// {
+//   PARALLEL = -1,
+//   INCOMING = 0,
+//   OUTGOING = 1
+// };
+
+// enum class AngleSetStatus
+// {
+//   NOT_FINISHED = 0,
+//   FINISHED = 1,
+//   RECEIVING = 2,
+//   READY_TO_EXECUTE = 3,
+//   EXECUTE = 4,
+//   NO_EXEC_IF_READY = 5,
+//   MESSAGES_SENT = 6,
+//   MESSAGES_PENDING = 7
+// };
+
+// struct Task
+// {
+//   unsigned int num_dependencies;
+//   std::vector<std::uint32_t> successors;
+//   uint64_t reference_id;
+//   const Cell* cell_ptr;
+//   bool completed = false;
+// };
+
+// /// Stage Task Dependency Graphs
+// struct STDG
+// {
+//   std::vector<int> item_id;
+// };
+
+// /// Communicates location by location dependencies.
+// void CommunicateLocationDependencies(const std::vector<int>& location_dependencies,
+//                                      std::vector<std::vector<int>>& global_dependencies);
+
+// /// Print a sweep ordering to file.
+// void PrintSweepOrdering(SPDS* sweep_order, std::shared_ptr<MeshContinuum> vol_continuum);
+
+// } // namespace opensn
+
 // SPDX-FileCopyrightText: 2024 The OpenSn Authors <https://open-sn.github.io/opensn/>
 // SPDX-License-Identifier: MIT
 
@@ -37,7 +96,9 @@ enum class AngleSetStatus
 struct Task
 {
   unsigned int num_dependencies;
+  std::vector<std::uint32_t> predecessors;
   std::vector<std::uint32_t> successors;
+  unsigned int num_satisfied_successors;
   uint64_t reference_id;
   const Cell* cell_ptr;
   bool completed = false;
